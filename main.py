fix Jacobian

main.py
@@ -6,8 +6,9 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
 from .models import *
-from .utils import new_predict
+from .utils import knn_predict, sigmoid
 import argparse
+from collections import Counter
 
 torch.manual_seed(42)
 
@@ -18,11 +19,10 @@ parser.add_argument('--batch_size', type=int, default=100)
 parser.add_argument('--lambd', type=float, default=0.0)
 parser.add_argument('--gamma', type=float, default=0.01,
                     help='gamma')
-parser.add_argument('--Nnoise', type=int, default=30,
-                    help='N samples to draw for epsilion')
+
 parser.add_argument('--code_size', type=int, default=60,
                     help='dimension of hidden layer')
-parser.add_argument('--code_size2', type=int, default=60,
+parser.add_argument('--code_size2', type=int, default=None,
                     help='dimension of hidden layer')
 
 parser.add_argument('--epsilon', type=float, default=0.1,
@@ -62,25 +62,49 @@ test_loader = torch.utils.data.DataLoader(dataset2, batch_size=args.batch_size)
 
 def cae_h_loss(imgs, imgs_noise,  recover, code_data, code_data_noise, lambd, gamma):
     criterion = nn.MSELoss()
-    loss1 = criterion(recover, imgs)
+    loss1=criterion(recover, imgs)
+    #incorrect:
+#     Jx = torch.autograd.grad(outputs=code_data, inputs=imgs, grad_outputs=torch.ones_like(
+#         code_data), create_graph=True)[0]
+#     Jx_noise = torch.autograd.grad(outputs=code_data_noise, inputs=imgs_noise,
+#                                    grad_outputs=torch.ones_like(code_data_noise), create_graph=True)[0]
+    
+    # old variant (slow):
+#     Jx=[]
+#     for i in range(batch_size):
+#         for j in range(code_data.shape[1]):
+#             Jx.append(torch.autograd.grad(outputs=code_data[i][j], inputs=imgs, retain_graph=True, create_graph=True)[0][i])
+#     Jx=torch.reshape(torch.cat(Jx),[batch_size, code_data.shape[1], imgs.shape[1]])
+    
+#     Jx_noise=[]
+#     for i in range(batch_size):
+#         for j in range(code_data.shape[1]):
+#             Jx_noise.append(torch.autograd.grad(outputs=code_data_noise[i][j], inputs=imgs_noise, retain_graph=True, create_graph=True)[0][i])
+#     Jx_noise=torch.reshape(torch.cat(Jx_noise),[batch_size, code_data_noise.shape[1], imgs_noise.shape[1]])
+    
+    
+    #new variant (faster)
+    grad_output=torch.ones(batch_size).cuda()
+    Jx=[]                                                                                        
+    for i in range(code_data.shape[1]):
+        Jx.append(torch.autograd.grad(outputs=code_data[:,i], inputs=imgs, grad_outputs=grad_output, retain_graph=True, create_graph=True)[0])
+    Jx=torch.reshape(torch.cat(Jx,1),[batch_size, code_data.shape[1], imgs.shape[1]])
+    
+    Jx_noise=[]                                                                                        
+    for i in range(code_data_noise.shape[1]):
+        Jx_noise.append(torch.autograd.grad(outputs=code_data_noise[:,i], inputs=imgs_noise, grad_outputs=grad_output, retain_graph=True, create_graph=True)[0])
+    Jx_noise=torch.reshape(torch.cat(Jx_noise,1),[batch_size, code_data_noise.shape[1], imgs_noise.shape[1]])
 
-    Jx = torch.autograd.grad(outputs=code_data, inputs=imgs, grad_outputs=torch.ones_like(
-        code_data), create_graph=True)[0]
-    Jx_noise = torch.autograd.grad(outputs=code_data_noise, inputs=imgs_noise,
-                                   grad_outputs=torch.ones_like(code_data_noise), create_graph=True)[0]
-
-    loss2 = torch.mean(torch.sum(torch.pow(Jx, 2), dim=[1, 2, 3]))
-    loss3 = torch.mean(torch.mean(
-        torch.sum(torch.pow(Jx - Jx_noise, 2), dim=[2, 3]), dim=1))
-
+    loss2 = torch.mean(torch.sum(torch.pow(Jx,2), dim=[1,2]))
+    loss3 = torch.mean(torch.sum(torch.pow(Jx - Jx_noise,2),dim=[1,2]))
     loss = loss1 + (lambd*loss2) + gamma*loss3
-
+    
     return loss
 
 if args.numlayers==1:
-    model = CAE1Layer(dimensionality, args.code_size, args.Nnoise)
+    model = CAE1Layer(dimensionality, args.code_size)
 elif args.numlayers==2:
-    model = CAE2Layer(dimensionality, [args.code_size, args.code_size2], args.Nnoise)
+    model = CAE2Layer(dimensionality, [args.code_size, args.code_size2])
 else:
     raise Exception("Sorry, numlayers only 1 or 2")
 
@@ -91,10 +115,9 @@ optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
 for i in range(args.epochs):
     train_loss = 0
     for step, (imgs, _) in enumerate(train_loader):
-        imgs = imgs.cuda()
+        imgs = imgs.view(batch_size, -1).cuda()
         imgs.requires_grad_(True)
-        imgs_noise = torch.autograd.Variable(imgs.data + torch.normal(0, args.epsilon, size=[
-                                             args.batch_size, args.Nnoise, image_size, image_size]).cuda(), requires_grad=True)
+        imgs_noise = torch.autograd.Variable(imgs.data + torch.normal(0, epsilon, size=[batch_size, dimensionality]).cuda(),requires_grad=True)
 
         recover, code_data, code_data_noise = model(imgs, imgs_noise)
         loss = cae_h_loss(imgs, imgs_noise, recover, code_data,
@@ -109,20 +132,25 @@ for i in range(args.epochs):
         optimizer.step()
 
         optimizer.zero_grad()
+
     if i % 10 == 0:
         print(i, train_loss/epoch_size)
 
 if args.save_dir_for_CAE:
     torch.save(model.state_dict(), args.save_dir_for_CAE)
 
+if args.MCT:
+    pass
 
 
 if args.KNN:
     dataset1 = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
     dataset2 = datasets.MNIST('data', train=False, download=True, transform=transforms.ToTensor())
 
-    dataset1 = torch.utils.data.Subset(dataset1, range(0, 1000))
-    dataset2 = torch.utils.data.Subset(dataset2, range(0, 10000))
+    test_size=10000
+    train_size=1000
+    dataset1=torch.utils.data.Subset(dataset1,range(0,train_size))
+    dataset2=torch.utils.data.Subset(dataset2,range(0,test_size))
     train_loader = torch.utils.data.DataLoader(dataset1, batch_size=len(dataset1))
     test_loader = torch.utils.data.DataLoader(dataset2, batch_size=len(dataset2))
 
@@ -131,31 +159,52 @@ if args.KNN:
     test_images = next(iter(test_loader))[0].numpy()
     test_labels = next(iter(test_loader))[1].numpy()
 
-    cur_W = model.W1.cpu().detach().numpy()
-    cur_b = model.b1.cpu().detach().numpy()
+    train_images=np.reshape(train_images, (train_size, -1))
+    test_images=np.reshape(test_images, (test_size, -1))
+
+    weights=None
+    if args.numlayers==1:
+        cur_W1 = model.W1.cpu().detach().numpy()
+        cur_b1 = model.b1.cpu().detach().numpy()
+        weights=[[cur_W1, cur_b1]]
+    elif args.numlayers==2:
+        cur_W1 = model.W1.cpu().detach().numpy()
+        cur_b1 = model.b1.cpu().detach().numpy()
+        cur_W2 = model.W2.cpu().detach().numpy()
+        cur_b2 = model.b2.cpu().detach().numpy()
+        weights=[[cur_W1, cur_b1],[cur_W2, cur_b2]]
+
+    #encode images
+    for W,b in weights:
+        train_images = sigmoid(np.matmul(train_images, W.T) + b)
+        test_images = sigmoid(np.matmul(train_images, W.T) + b)
 
     del model
     torch.cuda.empty_cache()
 
     # Predicting and printing the accuracy
-    for k in [1, 3,5]:
-        i = 0
-        total_correct = 0
-        for test_image in test_images:
-            pred = new_predict(k, train_images, train_labels, test_image)
-            if pred == test_labels[i]:
-                total_correct += 1
-            acc = (total_correct / (i+1)) * 100
-            if i % 2000 == 0:
-                print('test image['+str(i)+']', '\tpred:', pred, '\torig:',
-                      test_labels[i], '\tacc:', str(round(acc, 2))+'%')
-            i += 1
-        if args.numlayers==1:
-            with open('results2.txt','a') as f:
-                f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(args.learning_rate, args.lambd, args.gamma, args.code_size, args.epsilon, args.Nnoise, k, acc ))
+    
+    ks=np.arange(1,20,2)
 
-        elif args.numlayers==2:
-            with open('results2layers.txt','a') as f:
-                f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(args.learning_rate, args.lambd, args.gamma, args.code_size, args.code_size2, args.epsilon, args.Nnoise, k, acc ))
+    i = 0
+    total_correct={}
+    for k in ks:
+        total_correct[k]=0
+        
+    for test_image in test_images:
+        top_n_labels = knn_distances(train_images, train_labels, test_image, n_top=20)
+        for k in ks:
+            pred = Counter(top_n_labels[:k]).most_common(1)[0][0]
+            if pred == test_labels[i]:
+                total_correct[k] += 1
+        if i%4000 == 0:
+            print('test image['+str(i)+']')
+        i += 1
+
+    accuracies = {k: round((v/i) * 100, 2) for k,v in total_correct.items()}
+
+    for k in ks:
+        with open('results_CAEH.txt','a') as f:
+            f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(args.learning_rate, args.lambd, args.gamma, args.code_size, args.code_size2, args.epsilon, k, accuracies[k]))
 
 

utils.py
@@ -2,18 +2,15 @@ def sigmoid(x):
     return 1. / (1+np.exp(-x))
 
 
-def new_euclidean_distance(img_a, img_b):
-    img_a = np.reshape(img_a, (1, -1))
-    img_b = np.reshape(img_b, (1, -1))
-    img_a = sigmoid(np.matmul(img_a, cur_W) + cur_b)
-    img_b = sigmoid(np.matmul(img_b, cur_W) + cur_b)
+def euclidean_distance(img_a, img_b):
+    '''Finds the distance between 2 images: img_a, img_b'''
+    # element-wise computations are automatically handled by numpy
     return np.sum((img_a - img_b) ** 2)
 
 
-def predict(k, train_images, train_labels, test_images):
+def knn_distances(train_images, train_labels, test_image, n_top):
     '''
-    Predicts the new data-point's category/label by 
-    looking at all other training labels
+    returns n_top distances and labels for given test_image
     '''
     # distances contains tuples of (distance, label)
     distances = [(euclidean_distance(test_image, image), label)
@@ -22,46 +19,5 @@ def predict(k, train_images, train_labels, test_images):
 
     def compare(distance): return distance[0]
     by_distances = sorted(distances, key=compare)
-    # extract only k closest labels
-    k_labels = [label for (_, label) in by_distances[:k]]
-    # return the majority voted label
-    return find_majority(k_labels)
-
-
-def new_predict(k, train_images, train_labels, test_images):
-    '''
-    Predicts the new data-point's category/label by 
-    looking at all other training labels
-    '''
-    # distances contains tuples of (distance, label)
-    distances = [(new_euclidean_distance(test_image, image), label)
-                 for (image, label) in zip(train_images, train_labels)]
-    # sort the distances list by distances
-
-    compare = lambda distance: distance[0]
-    by_distances = sorted(distances, key=compare)
-    # extract only k closest labels
-    k_labels = [label for (_, label) in by_distances[:k]]
-    # return the majority voted label
-    return find_majority(k_labels)
-
-
-
-def euclidean_distance(img_a, img_b):
-    '''Finds the distance between 2 images: img_a, img_b'''
-    # element-wise computations are automatically handled by numpy
-    return np.sum((img_a - img_b) ** 2)
-    
-
-def find_majority(labels):
-    '''Finds the majority class/label out of the given labels'''
-    # defaultdict(type) is to automatically add new keys without throwing error.
-    counter = defaultdict(int)
-    for label in labels:
-        counter[label] += 1
-
-    # Finding the majority class.
-    majority_count = max(counter.values())
-    for key, value in counter.items():
-        if value == majority_count:
-            return key
+    top_n_labels = [label for (_, label) in distances[:n_top]]
+    return top_n_labels