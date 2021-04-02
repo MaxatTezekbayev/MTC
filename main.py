944d648b6d2311b80b4eb1ae1546da96ba663629
Maxat Tezekbayev <maksat013@gmail.com>
2021-04-03 00:30:56 +0600

    ALTER

--- main.py
+++ main.py
@@ -219,10 +219,9 @@ if args.ALTER:
                 _, code_data_noise = model(x_noise, calculate_jacobian=True)
                 _, code_data_z = model(z, calculate_jacobian=True)
                 
-                W1 = model.W1.clone()
-                W2 = model.W2.clone()
-                Jac = calc_jac(code_data, W1, W2)
-                Jac_noise = calc_jac(code_data_noise, W1, W2)
+
+                Jac = calc_jac(code_data, model.W1, model.W2)
+                Jac_noise = calc_jac(code_data_noise, model.W1, model.W2)
                 Jac_z = calc_jac(code_data_z, model.W1, model.W2)
                 loss, loss1 = alter_loss(x, recover, Jac, Jac_noise, Jac_z, b, args.lambd, args.gamma)
 
