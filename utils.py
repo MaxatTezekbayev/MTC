2f38dab185817378b2968ab6d919784eea951df2
Maxat Tezekbayev <maksat013@gmail.com>
2021-04-03 00:26:21 +0600

    ALTER

--- utils.py
+++ utils.py
@@ -104,10 +104,21 @@ def calculate_B_alter(model, train_z_loader, k, batch_size, first_time = False):
         print(step)
         z = z.view(batch_size, -1).cuda()
         z.requires_grad_(True)
-        recover_z, code_data_z = model(z, calculate_jacobian = True)
+        # recover_z, code_data_z = model(z, calculate_jacobian = True)
 
         W1 = model.W1.clone().detach().requires_grad_(True)
         W2 = model.W2.clone().detach().requires_grad_(True)
+        b1 = model.b1.clone().detach().requires_grad_(True)
+        b2 = model.b1.clone().detach().requires_grad_(True)
+        b3 = model.b1.clone().detach().requires_grad_(True)
+        b_r = model.b1.clone().detach().requires_grad_(True)
+
+        code_data1 = self.sigmoid(torch.matmul(z, W1.t()) + b1)
+        code_data2 = self.sigmoid(torch.matmul(code_data1, W2.t()) + b2)
+        #decode
+        code_data3 = self.sigmoid(torch.matmul(code_data2, W2) + b3)
+        recover = self.sigmoid(torch.matmul(code_data3, W1) + b_r)
+        code_data_z = [code_data1, code_data2, code_data3]
         Jac_z = calc_jac(code_data_z, W1, W2)
         u, sigma, v = torch.linalg.svd(Jac_z)
         if step==0:
