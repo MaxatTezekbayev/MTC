843b74d586dfa2974f1abfa0a9428c7f472ea23f
Maxat Tezekbayev <maksat013@gmail.com>
2021-04-02 17:34:44 +0600

    ALTER

--- utils.py
+++ utils.py
@@ -75,14 +75,14 @@ def calculate_B_alter(model, train_z_loader, k, batch_size, first_time = False):
     for step, (z, _) in enumerate(train_z_loader):
         z = z.view(batch_size, -1).cuda()
         z.requires_grad_(True)
-        recover_z, code_data_z  = model(z)
+        recover_z, code_data_z, Jac_z  = model(z, calculate_jacobian = True)
 
-        # Jac_z = Jacobian_for_ALTER(model, code_data_z)
-        grad_output=torch.ones(batch_size).cuda()
-        Jac_z=[]                                                                                        
-        for i in range(recover_z.shape[1]):
-            Jac_z.append(torch.autograd.grad(outputs=recover_z[:,i], inputs=z, grad_outputs=grad_output, retain_graph=True, create_graph=True)[0])
-        Jac_z=torch.reshape(torch.cat(Jac_z,1),[batch_size, recover_z.shape[1], z.shape[1]])
+        # # Jac_z = Jacobian_for_ALTER(model, code_data_z)
+        # grad_output=torch.ones(batch_size).cuda()
+        # Jac_z=[]                                                                                        
+        # for i in range(recover_z.shape[1]):
+        #     Jac_z.append(torch.autograd.grad(outputs=recover_z[:,i], inputs=z, grad_outputs=grad_output, retain_graph=True, create_graph=True)[0])
+        # Jac_z=torch.reshape(torch.cat(Jac_z,1),[batch_size, recover_z.shape[1], z.shape[1]])
     
 
         u, sigma, v = torch.svd(Jac_z)
