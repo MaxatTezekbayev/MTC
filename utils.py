03c03df1188cc3ae2ce46ab34d4cc28bb69ab3fa
Maxat Tezekbayev <maksat013@gmail.com>
2021-04-11 18:43:34 +0600

    B as constant

--- utils.py
+++ utils.py
@@ -71,7 +71,7 @@ def calculate_B_alter(model, train_z_loader, k, batch_size):
             z = z.view(batch_size, -1).cuda()
             _, code_data_z, Jac_z = model(z, calculate_jacobian = True)
             u, s, v = torch.svd(Jac_z.cpu())
-            b = torch.matmul(u[:, :, :k], torch.matmul(torch.diag_embed(s)[:, :k, :k], torch.transpose(v[:, :, :k],1,2)))
+            b = torch.matmul(u[:, :, :k].cuda(), torch.matmul(torch.diag_embed(s)[:, :k, :k].cuda(), torch.transpose(v[:, :, :k],1,2).cuda()))
             Bx.append(b)
     print("B time:", time.time() - start_time)
     return Bx
