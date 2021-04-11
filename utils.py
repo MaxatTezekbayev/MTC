4fd03a0ba0bec676ee394c7b7a1c6d85343d39fe
Maxat Tezekbayev <maksat013@gmail.com>
2021-04-11 21:07:29 +0600

    B as constant

--- utils.py
+++ utils.py
@@ -53,10 +53,15 @@ def MTC_loss(pred, y, u, imgs, beta, batch_size):
     loss=loss1 + beta * omega
     return loss, loss1
  
+# def svd_product(A, U, S, VH): # A*U*S*VH
+#     Q, R = torch.qr(torch.matmul(A.cuda(), U.cuda()).cpu())
+#     u_temp, s_temp, vh_temp = torch.svd(torch.matmul(R.cuda(), torch.diag(S).cuda()).cpu())
+#     return [torch.matmul(Q.cuda(), u_temp.cuda()), s_temp.cuda(), torch.matmul(vh_temp.T.cuda(), VH.cuda())]
+
 def svd_product(A, U, S, VH): # A*U*S*VH
-    Q, R = torch.qr(torch.matmul(A.cuda(), U.cuda()).cpu())
-    u_temp, s_temp, vh_temp = torch.svd(torch.matmul(R.cuda(), torch.diag(S).cuda()).cpu())
-    return [torch.matmul(Q.cuda(), u_temp.cuda()), s_temp.cuda(), torch.matmul(vh_temp.T.cuda(), VH.cuda())]
+    Q, R = torch.qr(torch.matmul(A, U))
+    u_temp, s_temp, vh_temp = torch.svd(torch.matmul(R, torch.diag(S)))
+    return [torch.matmul(Q, u_temp), s_temp, torch.matmul(vh_temp.T, VH)]
 
 def svd_drei(A, B, C, U, S, VH): # A*B*C*U*S*VH
     U1, S1, VH1 = svd_product(C, U, S, VH)
@@ -82,14 +87,14 @@ def calculate_B_alter(model, train_z_loader, k, batch_size, optimized_SVD):
             _, code_data_z, A_matrix, B_matrix, C_matrix = model(z, calculate_jacobian = False, calculate_DREI = True)
             U, S, VH = torch.svd(model.W1.clone().cpu())
             for i in range(len(A_matrix)):
-                u, s, vh = svd_drei(A_matrix[i], B_matrix[i], C_matrix[i], U, S, VH.T)
-                b = torch.matmul(u[:, :k].cuda(), torch.matmul(torch.diag_embed(s)[:k, :k].cuda(), vh[:k, :].cuda()))
+                u, s, vh = svd_drei(A_matrix[i].cpu(), B_matrix[i].cpu(), C_matrix[i].cpu(), U, S, VH.T)
+                b = torch.matmul(u[:, :k], torch.matmul(torch.diag_embed(s)[:k, :k], vh[:k, :]))
                 Bx_batch.append(b.cpu())
             Bx_batch = torch.stack(Bx_batch)
             # else:
             _, code_data_z, Jac_z = model(z, calculate_jacobian = True)
             U, S, V = torch.svd(Jac_z.cpu())
-            Bx_batch2 = torch.matmul(U[:, :, :k].cuda(), torch.matmul(torch.diag_embed(S)[:, :k, :k].cuda(), torch.transpose(V[:, :, :k],1,2).cuda())).cpu()
+            Bx_batch2 = torch.matmul(U[:, :, :k], torch.matmul(torch.diag_embed(S)[:, :k, :k], torch.transpose(V[:, :, :k],1,2)))
             print(Bx_batch.shape)
             print(Bx_batch2.shape)
             print(Bx_batch-Bx_batch2)
