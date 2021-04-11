81be7b8ae652b180534d749e50674333880da2a2
Maxat Tezekbayev <maksat013@gmail.com>
2021-04-11 19:30:44 +0600

    B as constant

--- utils.py
+++ utils.py
@@ -76,7 +76,7 @@ def calculate_B_alter(model, train_z_loader, k, batch_size):
             time_model.append(time.time() - start_time_model)
 
             start_time_svd = time.time()
-            u, s, v = torch.svd(Jac_z)
+            u, s, v = np.svd(Jac_z.cpu())
             time_svd.append(time.time() - start_time_svd)
 
             start_time_b = time.time()
